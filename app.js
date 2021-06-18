ad229057a395df7147690c168f0f81607a18277b
SherryCamp <77023078+SherryCamp@users.noreply.github.com>
2021-06-18 13:35:26 -0600

    Add files via upload
    commit1

---
+++ app.js
@@ -0,0 +1,96 @@
+// create a function that will slide the menu in on click 
+
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     // add the nav links query for animation
+//     const navLinks = document.querySelectorAll('.nav-links li');
+//         // togle nav
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+   
+//     //Animate links
+//         navLinks.forEach((link, index) => {
+//             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            
+//         });
+// // burger animation
+//  burger.classList.toggle('toggle');
+
+
+// });
+// } 
+// need to invoke the slide
+
+// navSlide();
+
+// end of navbar animation
+
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const projectsMenu = document.querySelector('#projects-page');
+  const galleryMenu = document.querySelector('#gallery-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+     galleryMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    galleryMenu.classList.remove('highlight');
+    projectsMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    projectsMenu.classList.add('highlight');
+    
+    aboutMenu.classList.remove('highlight');
+     galleryMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 6000) {
+    galleryMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+     projectsMenu.classList.remove('highlight');
+    return;
+  }
+  
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 980 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
